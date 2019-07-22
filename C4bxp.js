9f04a917d80d5ed23d85fc3542f5a664739faa39
YumiIwasa <yumi.iwasa@a-saas.com>
2019-07-22 22:44:51 +0900

    開眼！JavaScript第1章1.19

---
+++ C4bxp.js
@@ -0,0 +1,6 @@
+var CustomConstructor = function() { this.foo = 'bar'; }
+
+var instanceOfCustomObject = new CustomConstructor();
+console.log(instanceOfCustomObject instanceof CustomConstructor);
+
+console.log(new Array('foo') instanceof Array);
