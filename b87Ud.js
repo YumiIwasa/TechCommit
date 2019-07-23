71e798cdb38315da23758f9dab72d4025ac1530a
YumiIwasa <yumi.iwasa@a-saas.com>
2019-07-23 20:33:27 +0900

    開眼！JavaScript第2章2.1 オブジェクトはJavaScriptで表すことのできる全ての値を格納（もしくは参照）することができる。

---
+++ b87Ud.js
@@ -0,0 +1,51 @@
+var myObject = {};
+
+myObject.myFunction = function() {};
+myObject.myArray = [];
+myObject.myString = 'string';
+myObject.myNumber = 33;
+myObject.myDate = new Date();
+myObject.myRegExp = /a/;
+myObject.myNull = null;
+myObject.myUndefined = undefined;
+myObject.myObject = {};
+myObject.myMath_PI = Math.PI;
+myObject.myError = new Error('Error!');
+
+console.log(myObject.myFunction,
+    myObject.myArray,
+    myObject.myString,
+    myObject.myNumber,
+    myObject.myDate,
+    myObject.myRegExp,
+    myObject.myNull,
+    myObject.myUndefined,
+    myObject.myObject,
+    myObject.myMath_PI,
+    myObject.myError);
+
+var myFunction = function() {};
+
+myFunction.myFunction = function() {};
+myFunction.myArray = [];
+myFunction.myString = 'string';
+myFunction.myNumber = 33;
+myFunction.myDate = new Date();
+myFunction.myRegExp = /a/;
+myFunction.myNull = null;
+myFunction.myUndefined = undefined;
+myFunction.myObject = {};
+myFunction.myMath_PI = Math.PI;
+myFunction.myError = new Error('Error!');
+
+console.log(myFunction.myFunction,
+    myFunction.myArray,
+    myFunction.myString,
+    myFunction.myNumber,
+    myFunction.myDate,
+    myFunction.myRegExp,
+    myFunction.myNull,
+    myFunction.myUndefined,
+    myFunction.myObject,
+    myFunction.myMath_PI,
+    myFunction.myError);
